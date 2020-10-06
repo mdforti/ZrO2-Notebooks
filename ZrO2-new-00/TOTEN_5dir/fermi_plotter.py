c6e54669dbecb7b249e7cad3d181647e982e3abf
mariano Forti <marianodforti@gmail.com>
2020-10-06 18:34:39 -0300

    betterwall ultimo

--- ZrO2-new-00/TOTEN_5dir/fermi_plotter.py
+++ ZrO2-new-00/TOTEN_5dir/fermi_plotter.py
@@ -1,58 +1,16 @@
-#!/usr/bin/env python3
 from pymatgen.io.vasp.outputs import Vasprun, Eigenval
-from pymatgen.electronic_structure.boltztrap import BoltztrapRunner, BoltztrapAnalyzer
-from pymatgen.electronic_structure.plotter import plot_fermi_surface
+from ifermi.fermi_surface import FermiSurface
+from ifermi.plotter import FermiSurfacePlotter
+from ifermi.interpolator import Interpolater
 
-vrun = Vasprun('vasprun.xml')
-bs = vrun.get_band_structure()
-nelect = vrun.parameters['NELECT']
-vbm = int(nelect/2 - 1)
-BoltztrapRunner(
-        bs=bs,
-        nelec=nelect,
-        lpfac=100,
-        run_type='FERMI',
-        band_nb=25,
-        spin=1,
-        cond_band=True
-        ).run('./b25/')
-an = BoltztrapAnalyzer.from_files("b25/boltztrap/")
-plot_fermi_surface(
-        an.fermi_surface_data,
-        bs.structure,
-        cbm=True,
-        energy_levels=[0.2])
-BoltztrapRunner(bs=bs, nelec=nelec, type='BOLTZ').run(path_dir='full/')
-anfull = BoltztrapAnalyzer.from_files("full/")
-effmas_vs_t = anfull.get_average_eff_mass()
+vr = Vasprun('vasprun.xml')
+bs = vr.get_band_structure()
+interpolater=Interpolater(bs)
+interpolate_factor=3
+interp_bs, kpoint_dim = interpolater.interpolate_bands(interpolate_factor)
+fs = FermiSurface.from_band_structure(
+        interp_bs, kpoint_dim, mu=-0.2, wigner_seitz=True, 
+    )
+plotter = FermiSurfacePlotter(fs)
+plotter.plot(interactive=True, plot_type='mpl')
 
-
-#from pymatgen.io.vasp.outputs import Vasprun, Eigenval
-#from pymatgen.electronic_structure.boltztrap2 import *
-#from pymatgen.electronic_structure.boltztrap import BoltztrapRunner, BoltztrapAnalyzer
-# from pymatgen.electronic_structure.plotter import plot_fermi_surface
-#from monty.serialization import loadfn
-
-#data=VasprunLoader().from_file('vasprun.xml')
-#bs = loadfn(
-#vrun = Vasprun('vasprun.xml')
-#bs = vrun.get_band_structure()
-#nelect = vrun.parameters['NELECT']
-#vbm = int(nelect/2 - 1)
-#BoltztrapRunner(
-#        bs=bs,
-#        nelec=nelect,
-#        lpfac=100,
-#        run_type='FERMI',
-#        band_nb=vbm,
-#        cond_band=False,
-#        spin=1
-#        ).run(path_dir='vbm/')
-#
-#an = BoltztrapAnalyzer.from_files("vbm/boltztrap/")
-#plot_fermi_surface(
-#        an.fermi_surface_data,
-#        bs.structure,
-#        cbm=False,
-#        energy_levels=[-0.01]
-#        )
