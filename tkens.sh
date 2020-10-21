4368c7365a883094d5827367791fbc81ceb73ae1
mariano Forti <marianodforti@gmail.com>
2020-10-21 20:03:50 -0300

    fitted birch murnaghan

--- tkens.sh
+++ tkens.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-echo "dir Vol Evsk1 Evsk2 Evsk3 Evsk4 Evsk5" > datavol.dat
+echo "dir Vol Pressure Evsk1 Evsk2 Evsk3 Evsk4 Evsk5" > datavol.dat
 for dir in ZrO2-new-*
 do
   if [ ! -z $vol ]
@@ -7,11 +7,13 @@ do
     echo "unset vol="$vol
     unset vol
     unset E
+    unset pre
   fi
   vol=$(tar -xf $dir/TOTEN_3dir/OUT-OSZ-CON.tar.gz OUTCAR -O| grep volume | tail -1 | awk '{print $NF}')
+  pre=$(tar -xf $dir/TOTEN_3dir/OUT-OSZ-CON.tar.gz OUTCAR -O | grep pressure -i | awk '{print $4 / 10}')
   for i in 1 2 3 4 5
   do
      E=$E" "$(tar -xf $dir/TOTEN_"$i"dir/OUT-OSZ-CON.tar.gz OSZICAR -O | tail -1 | awk '{print $5}')
   done
-  echo "$dir $vol$E" | tee -a  datavol.dat
+  echo "$dir $vol $pre $E  " | tee -a  datavol.dat
 done
